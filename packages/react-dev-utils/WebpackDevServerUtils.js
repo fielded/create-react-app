4594dfd02afdd523ef1e80ea1e90c092ef938979
Frederik Ring <frederik.ring@gmail.com>
2019-05-27 15:35:36 +0200

    Always return `lanUrlForConfig`

--- packages/react-dev-utils/WebpackDevServerUtils.js
+++ packages/react-dev-utils/WebpackDevServerUtils.js
@@ -39,32 +39,28 @@ function prepareUrls(protocol, host, port) {
     });
 
   const isUnspecifiedHost = host === '0.0.0.0' || host === '::';
-  let prettyHost, lanUrlForConfig, lanUrlForTerminal;
-  if (isUnspecifiedHost) {
-    prettyHost = 'localhost';
-    try {
-      // This can only return an IPv4 address
-      lanUrlForConfig = address.ip();
-      if (lanUrlForConfig) {
-        // Check if the address is a private ip
-        // https://en.wikipedia.org/wiki/Private_network#Private_IPv4_address_spaces
-        if (
-          /^10[.]|^172[.](1[6-9]|2[0-9]|3[0-1])[.]|^192[.]168[.]/.test(
-            lanUrlForConfig
-          )
-        ) {
-          // Address is private, format it for later use
-          lanUrlForTerminal = prettyPrintUrl(lanUrlForConfig);
-        } else {
-          // Address is not private, so we will discard it
-          lanUrlForConfig = undefined;
-        }
+  let prettyHost = isUnspecifiedHost ? 'localhost' : host
+  let lanUrlForConfig, lanUrlForTerminal;
+  try {
+    // This can only return an IPv4 address
+    lanUrlForConfig = address.ip();
+    if (lanUrlForConfig) {
+      // Check if the address is a private ip
+      // https://en.wikipedia.org/wiki/Private_network#Private_IPv4_address_spaces
+      if (
+        /^10[.]|^172[.](1[6-9]|2[0-9]|3[0-1])[.]|^192[.]168[.]/.test(
+          lanUrlForConfig
+        )
+      ) {
+        // Address is private, format it for later use
+        lanUrlForTerminal = prettyPrintUrl(lanUrlForConfig);
+      } else {
+        // Address is not private, so we will discard it
+        lanUrlForConfig = undefined;
       }
-    } catch (_e) {
-      // ignored
     }
-  } else {
-    prettyHost = host;
+  } catch (_e) {
+    // ignored
   }
   const localUrlForTerminal = prettyPrintUrl(prettyHost);
   const localUrlForBrowser = formatUrl(prettyHost);
